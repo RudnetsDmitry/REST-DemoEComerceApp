96cfee5dfa257b50a0f551fcfd837d75f4d9c33a
DmitryR <r.mitya.79@gmail.com>
2019-01-09 23:42:19 +0300

    -add project structure

---
+++ DemoEComerceApp/Controllers/CustomersController.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using DemoEComerceApp.Model;
+using Microsoft.AspNetCore.Cors;
+
+namespace DemoEComerceApp.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/[controller]")]
+    [ApiController]
+    //[EnableCors("AllowAll")]
+    public class CustomersController : ControllerBase
+    {
+        private readonly EComerceDBContext _context;
+
+        public CustomersController(EComerceDBContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Customers
+        [HttpGet]
+        public IEnumerable<Customers> GetCustomers()
+        {
+            return _context.Customers;
+        }
+
+        // GET: api/Customers/5
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetCustomers([FromRoute] Guid id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var customers = await _context.Customers.FindAsync(id);
+
+            if (customers == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(customers);
+        }
+
+        // PUT: api/Customers/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutCustomers([FromRoute] Guid id, [FromBody] Customers customers)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != customers.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(customers).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!CustomersExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Customers
+        [HttpPost]
+        public async Task<IActionResult> PostCustomers([FromBody] Customers customers)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (_context.Customers.Any(x => x.Email == customers.Email))
+            {
+                ModelState.AddModelError("email", "User with mail id already exists!");
+                return BadRequest(ModelState);
+            }
+
+            _context.Customers.Add(customers);
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateException)
+            {
+                if (CustomersExists(customers.Id))
+                {
+                    return new StatusCodeResult(StatusCodes.Status409Conflict);
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return CreatedAtAction("GetCustomers", new { id = customers.Id }, customers);
+        }
+
+        // DELETE: api/Customers/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteCustomers([FromRoute] Guid id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var customers = await _context.Customers.FindAsync(id);
+            if (customers == null)
+            {
+                return NotFound();
+            }
+
+            _context.Customers.Remove(customers);
+            await _context.SaveChangesAsync();
+
+            return Ok(customers);
+        }
+
+        private bool CustomersExists(Guid id)
+        {
+            return _context.Customers.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ DemoEComerceApp/Interfaces/IProductService.cs
@@ -0,0 +1,13 @@
+﻿using DemoEComerceApp.Model;
+using System.Threading.Tasks;
+
+namespace DemoEComerceApp.Interfaces
+{
+    public interface IProductService
+    {
+        Task<bool> CreateProductAsync(Product product);
+        Task<Product> GetOrderAsync(int id);
+        Task<bool> UpdateProductAsync(int id, Product product);
+        Task<bool> DeleteOrderAsync(int id);
+    }
+}
